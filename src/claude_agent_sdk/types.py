539dc810c3f054ad3a6052bf8c2042749a4b25b0
shuofengzhang <sofuncheung16@gmail.com>
2026-03-02 16:40:36 +0800

    Refine permission control typing

--- src/claude_agent_sdk/types.py
+++ src/claude_agent_sdk/types.py
@@ -793,8 +793,7 @@ class SDKControlPermissionRequest(TypedDict):
     subtype: Literal["can_use_tool"]
     tool_name: str
     input: dict[str, Any]
-    # TODO: Add PermissionUpdate type here
-    permission_suggestions: list[Any] | None
+    permission_suggestions: list[PermissionUpdate] | None
     blocked_path: str | None
 
 
@@ -806,8 +805,7 @@ class SDKControlInitializeRequest(TypedDict):
 
 class SDKControlSetPermissionModeRequest(TypedDict):
     subtype: Literal["set_permission_mode"]
-    # TODO: Add PermissionMode
-    mode: str
+    mode: PermissionMode
 
 
 class SDKHookCallbackRequest(TypedDict):
